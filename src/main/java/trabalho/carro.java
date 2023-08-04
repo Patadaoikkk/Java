751a4bdbb855f95056842bb234b5ad30dfeea6e4
asdf <asdf>
2023-08-03 22:25:32 -0300

    oi

---
+++ src/main/java/trabalho/carro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalho;
+
+/**
+ *
+ * @author Admin
+ */
+public class carro {
+   String carr;
+ public carro(String carr){
+     this.carr = carr;
+ }  
+}
---
+++ src/main/java/trabalho/marca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalho;
+
+/**
+ *
+ * @author Admin
+ */
+
+public class marca extends carro{
+marca(String carr){
+        super(carr);
+    }
+}
